426cf0414fca68089ef3219fa04e75ff40b0fcb5
Selvendran <selvendrank@hotmail.com>
2024-12-04 21:39:57 +0530

    test

--- src/main/java/Pages/LeafPage.java
+++ src/main/java/Pages/LeafPage.java
@@ -11,37 +11,35 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LeafPage {
-  static WebDriver driver;
+	static WebDriver driver;
 
 	public static void main(String[] args) {
-		ChromeOptions chromeoption=new ChromeOptions();
-		  chromeoption.addArguments("--remote-allow-origins=*");
+		ChromeOptions chromeoption = new ChromeOptions();
+		chromeoption.addArguments("--remote-allow-origins=*");
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver(chromeoption);
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
-		
-		
 
- //driver.findElement(By.xpath("//*[text()='Open with delay']")).click();
- System.out.println(driver.getTitle());
- WebElement elt = driver.findElement(By.xpath("//*[text()='Images']"));
+		// driver.findElement(By.xpath("//*[text()='Open with delay']")).click();
+		System.out.println(driver.getTitle());
+		WebElement elt = driver.findElement(By.xpath("//*[text()='Images']"));
 
 //Get text 
- System.out.println(elt.getText());
- 
-/* Set <String> allWindowHandles = driver.getWindowHandles();
- String parentWindowHandle = driver.getWindowHandle();
+		System.out.println(elt.getText());
 
- for (String windowhandle: allWindowHandles)
- {
-   
-	if(!windowhandle.equals(parentWindowHandle)) {
-       
+		/*
+		 * Set <String> allWindowHandles = driver.getWindowHandles(); String
+		 * parentWindowHandle = driver.getWindowHandle();
+		 * 
+		 * for (String windowhandle: allWindowHandles) {
+		 * 
+		 * if(!windowhandle.equals(parentWindowHandle)) {
+		 * 
+		 * 
+		 * driver.switchTo().window(windowhandle); driver.close();
+		 */
 
- driver.switchTo().window(windowhandle);
- driver.close();*/ 
-		
-     }
+	}
 
 }
